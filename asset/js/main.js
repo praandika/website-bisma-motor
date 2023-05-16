deeb0c897fec2ec7f29ec64583b6bf819c81fa09
Andika Pranayoga <praandikayoga@gmail.com>
2023-05-16 15:46:25 +0800

    Update Featured

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,3 +10,93 @@ function scrollFunction() {
         document.getElementById("navigation").removeAttribute("class");
     }
 }
+
+function showSmartKey() {
+    let item = document.getElementById("smartKeyContent");
+    item.classList.remove("hide");
+}
+
+function hideSmartKey() {
+    let item = document.getElementById("smartKeyContent");
+    item.classList.add("class", "hide");
+}
+
+function showLuggage() {
+    let item = document.getElementById("luggageContent");
+    item.classList.remove("hide");
+}
+
+function hideLuggage() {
+    let item = document.getElementById("luggageContent");
+    item.classList.add("class", "hide");
+}
+
+function showFuel() {
+    let item = document.getElementById("smartFrontRefuelContent");
+    item.classList.remove("hide");
+}
+
+function hideFuel() {
+    let item = document.getElementById("smartFrontRefuelContent");
+    item.classList.add("class", "hide");
+}
+
+function showBlueCore() {
+    let item = document.getElementById("blueCoreContent");
+    item.classList.remove("hide");
+}
+
+function hideBlueCore() {
+    let item = document.getElementById("blueCoreContent");
+    item.classList.add("class", "hide");
+}
+
+function showSeat() {
+    let item = document.getElementById("seatContent");
+    item.classList.remove("hide");
+}
+
+function hideSeat() {
+    let item = document.getElementById("seatContent");
+    item.classList.add("class", "hide");
+}
+
+function showLed() {
+    let item = document.getElementById("ledContent");
+    item.classList.remove("hide");
+}
+
+function hideLed() {
+    let item = document.getElementById("ledContent");
+    item.classList.add("class", "hide");
+}
+
+function showFootStep() {
+    let item = document.getElementById("footstepContent");
+    item.classList.remove("hide");
+}
+
+function hideFootStep() {
+    let item = document.getElementById("footstepContent");
+    item.classList.add("class", "hide");
+}
+
+function showTire() {
+    let item = document.getElementById("tireContent");
+    item.classList.remove("hide");
+}
+
+function hideTire() {
+    let item = document.getElementById("tireContent");
+    item.classList.add("class", "hide");
+}
+
+function showSpeedo() {
+    let item = document.getElementById("speedoContent");
+    item.classList.remove("hide");
+}
+
+function hideSpeedo() {
+    let item = document.getElementById("speedoContent");
+    item.classList.add("class", "hide");
+}
